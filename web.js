72028c27362a7c4f1e5a3b02a5486f4c38decdd6
Philipp Bosch <hello@pb.io>
2012-10-24 19:15:15 +0200

    Refactor put route, add serveral commands. Add delete route.

--- web.js
+++ web.js
@@ -6,8 +6,6 @@ var express = require('express');
 var app = express();
 app.use(express.bodyParser());
 
-var port = process.env.PORT || 5000;
-
 app.get('/:key', function(req, res) {
     redis.get(req.params.key, function(err, value) {
         res.json({'value': value});
@@ -15,14 +13,53 @@ app.get('/:key', function(req, res) {
 });
 
 app.put('/:key', function(req, res) {
-    var input = req.body.value;
-    redis.set(req.params.key, input);
-    redis.get(req.params.key, function(err, value) {
-        res.json({'value': value});
-        pusher.trigger(req.params.key, 'set', {'value': value});
+    var key = req.params.key;
+
+    var callback = function() {
+        redis.get(key, function(err, value) {
+            res.json({'value': value});
+            pusher.trigger('updates', 'update:' + key, {'value': value});
+        });
+    };
+
+    if (req.body.value !== undefined) {
+        var input = req.body.value;
+        redis.set(key, input, callback);
+    } else if (req.body.action !== undefined) {
+        var action = req.body.action.toUpperCase();
+        var params = req.body.params || [];
+        var pl = params.length;
+
+        if (action == 'INCR') {
+            redis.incr(key, callback);
+        } else if (action == 'INCRBY' && pl == 1) {
+            redis.incrby(key, params[0], callback);
+        } else if (action == 'INCRBYFLOAT' && pl == 1) {
+            redis.incrbyfloat(key, params[0], callback);
+        } else if (action == 'DECR') {
+            redis.decr(key, callback);
+        } else if (action == 'DECRBY' && pl == 1) {
+            redis.decrby(key, params[0], callback);
+        } else if (action == 'APPEND' && pl == 1) {
+            redis.append(key, params[0], callback);
+        } else {
+            res.send(500);
+        }
+    }
+});
+
+app.delete('/:key', function(req, res) {
+    var key = req.params.key;
+    redis.get(key, function(err, value) {
+        redis.del(key, function(err, deleted) {
+            res.send(204);
+            pusher.trigger('updates', 'update:' + key, {'value': null});
+            pusher.trigger('deletes', 'delete:' + key, {'deleted': deleted, 'lastvalue': value});
+        });
     });
 });
 
+var port = process.env.PORT || 5000;
 app.listen(port, function() {
     console.log("Listening on " + port);
 });
