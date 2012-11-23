7b18eed338e85d7e1d276be0a1d614d4a41aeab1
Philipp Bosch <hello@pb.io>
2012-11-24 00:20:03 +0100

    Add hash capabilities. Better input handling.

--- web.js
+++ web.js
@@ -5,7 +5,22 @@ var pusher = require('pusher-url').connect(process.env.PUSHER_URL);
 
 var express = require('express');
 var app = express();
+app.use (function(req, res, next) {
+    if ('content-type' in req.headers && req.headers['content-type'].substr(0,5) == 'text/') {
+        var data='';
+        req.setEncoding('utf8');
+        req.on('data', function(chunk) {
+           data += chunk;
+        });
+
+        req.on('end', function() {
+            req.body = data;
+            next();
+        });
+    } else next();
+});
 app.use(express.bodyParser());
+
 
 /* Serve the JS client */
 app.get('/client.js', function(req, res) {
@@ -16,6 +31,7 @@ app.get('/client.js', function(req, res) {
         res.send(output);
     });
 });
+
 
 /* CORS */
 app.all('*', function(req, res, next) {
@@ -29,6 +45,7 @@ app.options('*', function(req, res, next) {
     res.send(200);
 });
 
+
 /* "Home page" */
 app.get('/', function(req, res) {
     res.json('Radiate is ready.');
@@ -36,9 +53,33 @@ app.get('/', function(req, res) {
 
 
 /* Getting data */
-app.get('/:key', function(req, res) {
-    redis.get(req.params.key, function(err, value) {
-        res.json({'value': value});
+function getData(key, field, callback) {
+    if (typeof field == 'function') {
+        callback = field;
+        delete field;
+    }
+    redis.type(key, function(err, type) {
+        if (type == 'string') {
+            redis.get(key, callback);
+        } else if (type == 'hash') {
+            if (field) {
+                redis.hget(key, field, callback);
+            } else {
+                redis.hgetall(key, callback);
+            }
+        } else if (type == 'none') {
+            callback('key not found');
+        }
+    });
+}
+
+app.get('/:key/:field?', function(req, res) {
+    getData(req.params.key, req.params.field, function(err, value) {
+        if (err) {
+            res.send(404);
+        } else {
+            res.json({'value': value});
+        }
     });
 });
 
@@ -46,37 +87,60 @@ app.get('/:key', function(req, res) {
 app.put('/:key', function(req, res) {
     var key = req.params.key;
 
-    var callback = function() {
-        redis.get(key, function(err, value) {
-            // TODO: error handling
-            res.json({'value': value});
-            pusher.trigger('updates', 'update:' + key, {'value': value});
-        });
+    var callback = function(err) {
+        if (err) {
+            res(500, err);
+        } else {
+            getData(key, function(err, value) {
+                res.json({'value': value});
+                pusher.trigger('updates', 'update:' + key, {'value': value});
+            });
+        }
     };
 
-    if (req.body.value !== undefined) {
-        var input = req.body.value;
+    if (typeof req.body == 'string') {
+        var input = req.body;
         redis.set(key, input, callback);
-    } else if (req.body.action !== undefined) {
-        var action = req.body.action.toUpperCase();
-        var params = req.body.params || [];
-        var pl = params.length;
+    } else if (typeof req.body == 'object') {
+        if ('_value' in req.body) {
+            var input = req.body._value;
+            if (typeof input != 'string') input = JSON.stringify(input);
+            redis.set(key, input, callback);
+        } else if ('_action' in req.body) {
+            var action = req.body._action.toUpperCase();
+            var params = req.body.params || [];
+            var pl = params.length;
 
-        if (action == 'INCR') {
-            redis.incr(key, callback);
-        } else if (action == 'INCRBY' && pl == 1) {
-            redis.incrby(key, params[0], callback);
-        } else if (action == 'INCRBYFLOAT' && pl == 1) {
-            redis.incrbyfloat(key, params[0], callback);
-        } else if (action == 'DECR') {
-            redis.decr(key, callback);
-        } else if (action == 'DECRBY' && pl == 1) {
-            redis.decrby(key, params[0], callback);
-        } else if (action == 'APPEND' && pl == 1) {
-            redis.append(key, params[0], callback);
+            if (action == 'INCR') {
+                redis.incr(key, callback);
+            } else if (action == 'INCRBY' && pl == 1) {
+                redis.incrby(key, params[0], callback);
+            } else if (action == 'INCRBYFLOAT' && pl == 1) {
+                redis.incrbyfloat(key, params[0], callback);
+            } else if (action == 'DECR') {
+                redis.decr(key, callback);
+            } else if (action == 'DECRBY' && pl == 1) {
+                redis.decrby(key, params[0], callback);
+            } else if (action == 'APPEND' && pl == 1) {
+                redis.append(key, params[0], callback);
+            } else {
+                res.send(500);
+            }
         } else {
-            res.send(500);
+            var fields = {};
+            for (var field in req.body) {
+                if (req.body.hasOwnProperty(prop)) {
+                    if (typeof req.body[field] == 'string') {
+                        fields[field] = req.body[field];
+                    } else {
+                        fields[field] = JSON.stringify(req.body[field]);
+                    }
+                }
+            }
+            redis.hmset(key, fields, callback);
         }
+    } else {
+        res.send(500);
     }
 });
 
